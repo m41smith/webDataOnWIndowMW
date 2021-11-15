66e0a79c83ee6da9b1bc65700a3e0ae8a902b6ec
Marcus <m41smith@student.bridgew.edu>
2021-11-15 15:43:28 -0500

    -Got a working program

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module bsu.comp152.webdataonwindowmw {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+    requires java.net.http;
+    requires com.google.gson;
+
+    opens bsu.comp152.webdataonwindowmw to javafx.fxml, com.google.gson;
+    exports bsu.comp152.webdataonwindowmw;
+}
